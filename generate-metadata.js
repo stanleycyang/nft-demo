1fc3004c10c8e8b53ab95deca98508493773519b
Stanley Yang <me@stanleycyang.com>
2021-09-20 23:07:59 -0700

    Updated code

--- generate-metadata.js
+++ generate-metadata.js
@@ -1,6 +1,20 @@
 const fs = require("fs");
 const weighted = require("weighted");
 const _ = require("lodash");
+const path = require("path");
+
+const NAME = "Smiley Facez NFT #";
+const SYMBOL = "SFZ";
+const DESCRIPTION = "Bringing joy through smiley facez to the world!";
+const SELLER_FEE_BASIS_POINTS = 500;
+const CREATORS = [
+  { address: "2cJJk4N1LAYd1mKHpL4Tu7UsB77KBM5cbHNVktVi3djn", share: 100 },
+];
+const COLLECTION = {
+  name: "Smiley Facez NFT",
+  family: "Smiley Facez",
+};
+const NUMBER_OF_IMAGES_TO_CREATE = 100;
 
 const generateRandomSet = (breakdown, id) => {
   const tmp = {};
@@ -12,9 +26,31 @@ const generateRandomSet = (breakdown, id) => {
   return tmp;
 };
 
+const createMetadata = (attrs, index) => {
+  const attributes = [];
+
+  for (const prop in attrs) {
+    attributes.push({
+      trait_type: prop,
+      value: path.parse(attrs[prop]).name,
+    });
+  }
+
+  const metadata = {
+    name: `${NAME}${index + 1}`,
+    symbol: SYMBOL,
+    description: DESCRIPTION,
+    seller_fee_basis_points: SELLER_FEE_BASIS_POINTS,
+    attributes,
+    collection: COLLECTION,
+    creators: CREATORS,
+  };
+
+  fs.writeFileSync(`./assets/${index}.json`, JSON.stringify(metadata));
+};
+
 const main = () => {
-  const metadata = [];
-  const NUMBER_OF_IMAGES_TO_CREATE = 10000;
+  const randomSets = [];
 
   let numberOfImagesCreated = 0;
   const { breakdown } = JSON.parse(fs.readFileSync("./config.json", "utf-8"));
@@ -23,18 +59,19 @@ const main = () => {
     const randomSet = generateRandomSet(breakdown, numberOfImagesCreated + 1);
 
     // ensure sets are completely unique
-    if (!_.some(metadata, randomSet)) {
-      metadata.push(randomSet);
+    if (!_.some(randomSets, randomSet)) {
+      createMetadata(randomSet, numberOfImagesCreated);
+      randomSets.push(randomSet);
       numberOfImagesCreated += 1;
     }
   }
 
-  const metadataWithId = metadata.map((item, index) => ({
+  const randomSetsWithId = randomSets.map((item, index) => ({
     id: index + 1,
     ...item,
   }));
 
-  fs.writeFileSync("./metadata.json", JSON.stringify(metadataWithId));
+  fs.writeFileSync("./random-sets.json", JSON.stringify(randomSetsWithId));
 };
 
 main();
--- generate-nfts.js
+++ generate-nfts.js
@@ -1,11 +1,12 @@
-const fs = require("fs");
-const { createCanvas, loadImage } = require("canvas");
-// const mergeImages = require("merge-images");
-// const { Image, Canvas } = require("canvas");
-// const ImageDataURI = require("image-data-uri");
+import fs from "fs";
+import canvas from "canvas";
+import imagemin from "imagemin";
+import imageminPngquant from "imagemin-pngquant";
+
+const { createCanvas, loadImage } = canvas;
 
 const TRAITS_DIRECTORY = "./traits";
-const OUTPUT_DIRECTORY = "./outputs";
+const OUTPUT_DIRECTORY = "./assets";
 const WIDTH = 1000;
 const HEIGHT = 1000;
 
@@ -16,6 +17,7 @@ function sleep(ms) {
 const createImage = async (order = [], image) => {
   const canvas = createCanvas(WIDTH, HEIGHT);
   const context = canvas.getContext("2d");
+  const ID = parseInt(image.id, 10) - 1;
   await Promise.all(
     order.map(async (cur) => {
       const imageLocation = `${TRAITS_DIRECTORY}/${cur}/${image[cur]}`;
@@ -27,16 +29,23 @@ const createImage = async (order = [], image) => {
   );
 
   const buffer = canvas.toBuffer("image/png");
-  fs.writeFileSync(`${OUTPUT_DIRECTORY}/${image.id}.png`, buffer);
+  const optimizedImage = await imagemin.buffer(buffer, {
+    plugins: [
+      imageminPngquant({
+        quality: [0.6, 0.8],
+      }),
+    ],
+  });
+  fs.writeFileSync(`${OUTPUT_DIRECTORY}/${ID}.png`, optimizedImage);
 };
 
 const main = () => {
   const { order } = JSON.parse(fs.readFileSync("./config.json"));
-  const metadata = JSON.parse(fs.readFileSync("./metadata.json"));
+  const randomSets = JSON.parse(fs.readFileSync("./random-sets.json"));
   const PROCESSING_LENGTH = 50;
 
   const processImage = async (marker = 0) => {
-    const slice = metadata.slice(marker, marker + PROCESSING_LENGTH + 1);
+    const slice = randomSets.slice(marker, marker + PROCESSING_LENGTH + 1);
 
     // generate images for the portion
     await Promise.all(
@@ -48,26 +57,11 @@ const main = () => {
     marker += PROCESSING_LENGTH;
     console.log(`Completed up to ${marker}, sleeping..`);
     await sleep(1000);
-    if (marker < metadata.length - 1) processImage(marker);
+    if (marker < randomSets.length - 1) processImage(marker);
   };
 
+  // recurse until completion
   processImage();
-
-  // metadata.map(async (image, index) => {
-  //   const ID = index + 1;
-  //   await createImage(order, ID, image);
-  //   console.log(`COMPLETED ${ID}`);
-  // });
-
-  // metadata.map(async (image, index) => {
-  //   const ID = index + 1;
-  //   const stackedRankedImages = order.map((cur) => {
-  //     return `${TRAITS_DIRECTORY}/${cur}/${image[cur]}`;
-  //   });
-  //   const b64 = await mergeImages(stackedRankedImages, { Canvas, Image });
-  //   await ImageDataURI.outputFile(b64, `${OUTPUT_DIRECTORY}/${ID}.png`);
-  //   console.log(`COMPLETED ${ID}`);
-  // });
 };
 
 main();
